e40cb1e0a8529dc7ce819be6d952ff73711d4ac1
gitzaid01 <zaidsiddiqui546@gmail.com>
2026-07-14 22:27:25 +0530

    server.js updated

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ dotenv.config();
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
-connectDB();
-
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to the database:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
