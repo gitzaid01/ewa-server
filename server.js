b2b5eff4840c12f1860417be969b3c4803faeff7
gitzaid01 <zaidsiddiqui546@gmail.com>
2026-07-14 22:23:29 +0530

    Initial commit

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ import connectDB from "./config/db.js";
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
