636b56a36f67e4306fc2487b964ce36a8c1bdb65
gitzaid01 <zaidsiddiqui546@gmail.com>
2026-07-15 16:47:26 +0530

    get product by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -147,3 +147,29 @@ export const updateProduct = async (req, res) => {
     });
   }
 };
+
+
+export const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { addProduct, getProducts, updateProduct } from "../controllers/productController.js";
+import { addProduct, getProducts, updateProduct, getProductById } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.post("/add", addProduct);
 router.get("/", getProducts);
+router.get("/:id", getProductById);
 router.put("/:id", updateProduct);
 
 export default router;
